cc3e391135f337bface93c84ca55f42a18bca251
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-02 15:13:36 +0600

    test

--- MIlestone.js
+++ MIlestone.js
@@ -3,8 +3,16 @@ var price = 20;
 var dam = '50'
 
 
+var test = dam == price;
 
-console.log(price++);
-console.log(price);
+var six = 6;
+var xis = 6;
 
+
+
+// var result = cond1 === cond2; //true
+
+// console.log(result);  //true
+
+console.log(!(six == xis)); 
 
