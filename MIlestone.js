9f53efb207506e24fa6842b67857764033bb2cca
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-03 17:50:37 +0600

    Game

--- MIlestone.js
+++ MIlestone.js
@@ -21,15 +21,38 @@
 //     console.log(i, student[i]);
 // }
 
-for (let num = 0; num <= 100; num++ ){
-   let even 
-   if(num%2 !==0 && num>0){
-    even = num
-    console.log("Odd Number =", even);
-   }
+// for (let num = 0; num <= 100; num++ ){
+//    let even 
+//    if(num%2 !==0 && num>0){
+//     even = num
+//     console.log("Odd Number =", even);
+//    }
    
-}
+// }
 
+
+
+ 
+// let score = 75;
+// let grade;
+// if (score >= 90) {
+//  grade = "A";
+// } else if (score >= 80) {
+//  grade = "B";
+// }
+// console.log(grade);
+
+
+let gus = prompt("Gees a Number in 1 to 5")
+
+let result = 2;
+
+if(gus == result){
+    console.log("Yes You are Right");
+}
+else{
+    console.log("Not this . Plz think again");
+}
 
 
 
