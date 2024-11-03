dbf41fee6cd85c567d9fd7a2dab8e99b09018e77
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-03 10:19:32 +0600

    while

--- MIlestone.js
+++ MIlestone.js
@@ -66,30 +66,38 @@
 
 
 
-var mark = prompt("Mark")
-var grade;
+// var mark = prompt("Mark")
+// var grade;
 
-if(mark >= 80 && mark <= 100){
-    grade = "A+"
-}else if(mark >= 70 && mark <= 79){
-    grade = "A-"
-}else if(mark >= 60 && mark <= 69){
-    grade = "B"
-}else if(mark >= 50 && mark <= 59){
-    grade = "C"
-}else if(mark >= 40 && mark <=49){
-    grade = "D"
-}else if(mark >= 33 && mark <= 39){
-    grade = "Pass"
-}else{
-    grade = "Your Not eligible for result"
+// if(mark >= 80 && mark <= 100){
+//     grade = "A+"
+// }else if(mark >= 70 && mark <= 79){
+//     grade = "A-"
+// }else if(mark >= 60 && mark <= 69){
+//     grade = "B"
+// }else if(mark >= 50 && mark <= 59){
+//     grade = "C"
+// }else if(mark >= 40 && mark <=49){
+//     grade = "D"
+// }else if(mark >= 33 && mark <= 39){
+//     grade = "Pass"
+// }else{
+//     grade = "Your Not eligible for result"
+// }
+
+// console.log(grade);
+
+// for (let love = 1; love <=8; love++){
+//     console.log("I love you Jan", love);
+// }
+
+
+let love = 1;
+
+while(love <= 8){
+    console.log("I love You Jan");
+    love++
 }
-
-console.log(grade);
-
-
-
-
 
 
 
