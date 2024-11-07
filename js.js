bd54d086deb7ace6e6a55fe664711c2d3aac29ee
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-07 20:29:06 +0600

    math in let functon

--- js.js
+++ js.js
@@ -1,11 +1,8 @@
 // let nam = prompt("Enter Your Name")
-
 // function myFunction(nam){
 //     console.log("Hello " +nam);
 // }
 // myFunction(nam)
-
-
 // function myFunction(){
 //     console.log("Subscribed");
 // }
@@ -14,34 +11,30 @@
 //     console.log("Unsubscribed");
 // }
 // document.getElementById('btn2').addEventListener('click', bal)
-
 // let nam = prompt("Enter Your Name")
 // function myName(nam){
 //     console.log("My name is " +nam);
 // }
 // myName(nam)
-
 // function subcribe() {
 //     console.log("You Subscribed");
 // }
 // document.getElementById('btn').addEventListener("click", subcribe)
-
-
 // function Unsubscribed() {
 //     console.log("You Unsubscribed");
 // }
 // document.getElementById('btn2').addEventListener("click", Unsubscribed)
-(function(nzm){
-    console.log("I am self Invoking Function " + nzm);
-})
-("ga")
+// (function(nzm){
+//     console.log("I am self Invoking Function " + nzm);
+// })
+// ("self")
 
-
-
-
-
-
-
+let math = function(x, y){
+    return x * y
+}
+console.log(math(4,8));
+console.log(math(3,8));
+console.log(math(2,8));
 
 
 
