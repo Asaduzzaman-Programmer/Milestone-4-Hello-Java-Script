3f67ddddb63aa2f75fb50d75ba1f3edc022d95d5
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-07 20:08:28 +0600

    Combination of Subscribed and Unsubscribed $ Name input

--- js.js
+++ js.js
@@ -6,35 +6,31 @@
 // myFunction(nam)
 
 
-function myFunction(){
-    console.log("Subscribed");
+// function myFunction(){
+//     console.log("Subscribed");
+// }
+// document.getElementById('btn').addEventListener("click", myFunction)
+// function bal(){
+//     console.log("Unsubscribed");
+// }
+// document.getElementById('btn2').addEventListener('click', bal)
+
+let nam = prompt("Enter Your Name")
+function myName(nam){
+    console.log("My name is " +nam);
 }
+myName(nam)
 
-document.getElementById('btn').addEventListener("click", myFunction)
-
-
-
-
-
-
+function subcribe() {
+    console.log("You Subscribed");
+}
+document.getElementById('btn').addEventListener("click", subcribe)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function Unsubscribed() {
+    console.log("You Unsubscribed");
+}
+document.getElementById('btn2').addEventListener("click", Unsubscribed)
 
 
 
