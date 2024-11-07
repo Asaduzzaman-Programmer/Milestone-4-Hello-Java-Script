2a8198ff530d8d6f94028b48be688a7230c7c235
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-07 20:17:15 +0600

    Self Invoking Function

--- js.js
+++ js.js
@@ -15,25 +15,26 @@
 // }
 // document.getElementById('btn2').addEventListener('click', bal)
 
-let nam = prompt("Enter Your Name")
-function myName(nam){
-    console.log("My name is " +nam);
-}
-myName(nam)
+// let nam = prompt("Enter Your Name")
+// function myName(nam){
+//     console.log("My name is " +nam);
+// }
+// myName(nam)
 
-function subcribe() {
-    console.log("You Subscribed");
-}
-document.getElementById('btn').addEventListener("click", subcribe)
-
-
-function Unsubscribed() {
-    console.log("You Unsubscribed");
-}
-document.getElementById('btn2').addEventListener("click", Unsubscribed)
+// function subcribe() {
+//     console.log("You Subscribed");
+// }
+// document.getElementById('btn').addEventListener("click", subcribe)
 
 
-
+// function Unsubscribed() {
+//     console.log("You Unsubscribed");
+// }
+// document.getElementById('btn2').addEventListener("click", Unsubscribed)
+(function(nzm){
+    console.log("I am self Invoking Function " + nzm);
+})
+("ga")
 
 
 
