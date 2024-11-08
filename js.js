0ece461b5287147e08d1aba6971dd6ed43cd4fde
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 12:20:45 +0600

    Even Number

--- js.js
+++ js.js
@@ -17,8 +17,23 @@
 //     console.log("Subscribed"); 
 // }
 // document.getElementById('btn').addEventListener('click', myFunction)
+// let num = 1;
+// let sum = 0
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log(sum);
+//     num++
+// }
 
-
+let num = 1;
+while(num <= 10){
+    console.log(num);
+    if(num % 2 === 0){
+        console.log("Even Number", num);
+    }
+    num++
+}
 
 
 
