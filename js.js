8fb3a3a7b65e5c7b11ab53eceaef6cbbf9ba8765
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 14:48:39 +0600

    continue reverse

--- js.js
+++ js.js
@@ -5,7 +5,6 @@
 //     }
 // }
 // console.log("Break");
-
 // let num = 50;
 // while(num < 54){
 //     console.log(num);
@@ -14,15 +13,20 @@
 //         break
 //     }
 // }
-
-for(let i = 1; i <= 20; i++){ 
-    if(i % 2 === 1){
+// for(let i = 1; i <= 20; i++){ 
+//     if(i % 2 === 1){
+//         continue;
+//     }
+//     console.log(i);
+// }
+let num = 50;
+while(num <= 60){
+    num++;
+    if(num % 5 === 0){
         continue;
     }
-    console.log(i);
+    console.log(num);
 }
-
-
 
 
 
