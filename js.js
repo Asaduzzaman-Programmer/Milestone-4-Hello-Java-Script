472de6917fcb315cd225fcfa43b9bb7c4d791456
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 14:16:23 +0600

    odd and sum

--- js.js
+++ js.js
@@ -1,14 +1,20 @@
-for(let num = 0; num<=20; num++){
-    
-    if(num % 2 !==0){
-        console.log("Odd Number", num);
+// for(let num = 0; num<=20; num++){
+//     if(num % 2 !==0){
+//         console.log("Odd Number", num);
+//     }
+// }
+// for(let num = 1; num<= 30; num++){
+//     if(num % 3 ===0 && num % 5===0){
+//         console.log(num);
+//     }
+// }
+let sum = 0;
+for(let num = 1; num <= 20; num++){
+    if(num % 3 === 0){
+        sum = sum + num          
     }
 }
-
-
-
-
-
+console.log(sum);   
 
 
 
