8373957051ee0b8e94bc343d0ea58425b61b8e5e
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 12:34:59 +0600

    for even

--- MIlestone.js
+++ MIlestone.js
@@ -256,6 +256,47 @@
 //     alert("hello " + inpu);
 // }
 // myFunction()
+// const nums = [1,2,3,4,5,6,7];
+// for(const num of nums){
+//     console.log(num);
+// }
+// for (const num in nums){
+//     console.log(nums[num]);
+// }
+// nums.forEach(function(num){
+//     console.log(num);
+// })
+// let nam = 0;
+// while(nam < 4){
+//     console.log("Taskin I love you", nam);
+//     nam++
+// }
+// function myFunction(){
+//     console.log("Subscribed"); 
+// }
+// document.getElementById('btn').addEventListener('click', myFunction)
+// let num = 1;
+// let sum = 0
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log(sum);
+//     num++
+// }
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     if(num % 2 === 0){
+//         console.log("Even Number", num);
+//     }
+//     num++
+// }
+// for (let i = 1; i <= 200; i++) {
+//     console.log(i);
+//     if (i === 100) {
+//       break; // ১০০ পাওয়া গেলে লুপ বন্ধ হবে
+//     }
+//   }
  
 
 
