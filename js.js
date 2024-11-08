0877e148cbd39de978dd24d6933ccbe9e7bb8c96
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 11:51:02 +0600

    while  loop

--- js.js
+++ js.js
@@ -1,12 +1,19 @@
+// const nums = [1,2,3,4,5,6,7];
+// for(const num of nums){
+//     console.log(num);
+// }
+// for (const num in nums){
+//     console.log(nums[num]);
+// }
+// nums.forEach(function(num){
+//     console.log(num);
+// })
 
-
-
-
-
-
-
-
-
+let nam = 0;
+while(nam < 4){
+    console.log("Taskin I love you", nam);
+    nam++
+}
 
 
 
