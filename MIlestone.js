1506af645538908f71a7f28bd52ce05d9324ec5b
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 14:06:29 +0600

    Odd Number

--- MIlestone.js
+++ MIlestone.js
@@ -297,6 +297,26 @@
 //       break; // ১০০ পাওয়া গেলে লুপ বন্ধ হবে
 //     }
 //   }
+// for(let i = 0; i <= 10; i++){
+//     if(i % 2 === 0){
+//         console.log('Even Number', i);
+//     }
+// }
+  
+// let sum = 0;
+// for (let i = 11; i <= 20; i++){
+//     sum = sum + i;
+//     console.log(sum);
+// }
+// for (let i = 20; i >= 10; i--){
+//     console.log(i);
+// }
+// let i = 10;
+// while(i >=0){
+//     console.log(i);
+//     i--
+// }
+
  
 
 
--- js.js
+++ js.js
@@ -1,26 +1,9 @@
-// for(let i = 0; i <= 10; i++){
-//     if(i % 2 === 0){
-//         console.log('Even Number', i);
-//     }
-// }
-  
-// let sum = 0;
-// for (let i = 11; i <= 20; i++){
-//     sum = sum + i;
-//     console.log(sum);
-// }
-// for (let i = 20; i >= 10; i--){
-//     console.log(i);
-// }
-// let i = 10;
-// while(i >=0){
-//     console.log(i);
-//     i--
-// }
-
-
-
-
+for(let num = 0; num<=20; num++){
+    
+    if(num % 2 !==0){
+        console.log("Odd Number", num);
+    }
+}
 
 
 
