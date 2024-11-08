aab6798bfadacce0d05fb34bbea2fac6dede243d
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-08 12:47:23 +0600

    decrement

--- js.js
+++ js.js
@@ -1,16 +1,22 @@
-for(let i = 0; i <= 10; i++){
-    if(i % 2 === 0){
-        console.log('Even Number', i);
-    }
-}
+// for(let i = 0; i <= 10; i++){
+//     if(i % 2 === 0){
+//         console.log('Even Number', i);
+//     }
+// }
   
-
-
-
-
-
-
-
+// let sum = 0;
+// for (let i = 11; i <= 20; i++){
+//     sum = sum + i;
+//     console.log(sum);
+// }
+// for (let i = 20; i >= 10; i--){
+//     console.log(i);
+// }
+// let i = 10;
+// while(i >=0){
+//     console.log(i);
+//     i--
+// }
 
 
 
