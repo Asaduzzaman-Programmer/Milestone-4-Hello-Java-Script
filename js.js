a9ecd8d37056bd131a4286551eebb8b229e3fc9e
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-09 18:31:39 +0600

    split

--- js.js
+++ js.js
@@ -1,3 +1,42 @@
-const distict = `123456789`;
-const show = distict.slice(0, 3);
+// const distict = `123456789`;
+// const show = distict.slice(0, 3);
+// console.log(show);
+
+const nam = "I am a bad person";
+console.log(nam);
+const res = nam.split(" ");
+console.log(res);
+const friends = "Rahim,Karim,Mukul,Rabbi";
+const show = friends.split(",")
 console.log(show);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
