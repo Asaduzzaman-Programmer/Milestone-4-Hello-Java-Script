7cf81aa35bcb8192d7a749426b225363fb069532
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-10 21:25:51 +0600

    nested

--- js.js
+++ js.js
@@ -2,12 +2,20 @@ let person = {
     name: "sodor uddin",
     profession: "developer",
     sallary: 20000,
+    uniqe: {
+        color: "vivid",
+        result: {
+            gpa: 5,
+            viw: {
+                theme: "blavck"
+            }
+        }
+    }
 }
 let keys = Object.keys(person)
 console.log(keys);
 console.log(Object.values(person));
-
-
+console.log(person.uniqe.result.viw.theme);
 
 
 
