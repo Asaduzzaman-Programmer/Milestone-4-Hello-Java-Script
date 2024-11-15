1450db175313d5d7201e72f43508c996cf3a8825
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-15 23:37:26 +0600

    btn2

--- MIlestone.js
+++ MIlestone.js
@@ -427,6 +427,16 @@
 // for (let i of faka){
 //     console.log(i);
 // }
+// let num = ["Tom", "Tim", "Tin", "Tik"];
+// for (let i = num.length-1; i>=0; i--){
+//     let j = num[i]
+//     console.log(j);
+
+//     let re = num.join(" ")
+//     console.log(re);
+// }
+// var b = 4;
+// console.log(b);
 
 
 
--- js.js
+++ js.js
@@ -1,16 +1,13 @@
-let num = ["Tom", "Tim", "Tin", "Tik"];
-for (let i = num.length-1; i>=0; i--){
-    let j = num[i]
-    console.log(j);
-
-    let re = num.join(" ")
-    console.log(re);
+function subs(){
+    console.log("Subscribe");
 }
-var b = 4;
-console.log(b);
 
+function unsubs(){
+    console.log("Unsubsribe");
+}
 
-
+document.getElementById('btn').addEventListener('click', subs)
+document.getElementById('btn2').addEventListener('click', unsubs)
 
 
 
