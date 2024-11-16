fa6ff504544aaae1079f881e96d9ab3c599983f3
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-17 02:59:53 +0600

    if func

--- js.js
+++ js.js
@@ -1,17 +1,25 @@
-function res (main){
-    let start = 0;
-    for(num of main){
-        console.log(num);
-        start = start + num;
+function sum(array){
+    let number = 0;
+    for(i of array){
+        number = number + i
     }
-    return start
+    return number
 }
-const main = [1,2,3,4,5]
-const total = res(main)
-console.log("The sum of the array is", total);
-
-
-
+const array = [1,2,3,4,5,6]
+const result = sum(array);
+console.log("The sum of full array :",result);
+function even(arry){
+    let num = 0;
+    for(i of arry){
+        if(i % 2 ===0){
+            num = num + i
+        } 
+    }
+    return num;
+}
+const arry = [1,2,3,4,5,6]
+const resul = even(arry);
+console.log("The sum of EvenNumbers :", resul);
 
 
 
