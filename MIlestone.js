feeacb67abf4d08cc7bfef3ccc85d72ef0d90a31
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-17 03:15:38 +0600

    inch to feet

--- MIlestone.js
+++ MIlestone.js
@@ -464,6 +464,28 @@
 //     return false;
 // }
 // console.log(test(1));
+// function sum(array){
+//     let number = 0;
+//     for(i of array){
+//         number = number + i
+//     }
+//     return number
+// }
+// const array = [1,2,3,4,5,6]
+// const result = sum(array);
+// console.log("The sum of full array :",result);
+// function even(arry){
+//     let num = 0;
+//     for(i of arry){
+//         if(i % 2 ===0){
+//             num = num + i
+//         } 
+//     }
+//     return num;
+// }
+// const arry = [1,2,3,4,5,6]
+// const resul = even(arry);
+// console.log("The sum of EvenNumbers :", resul);
 
 
 
--- js.js
+++ js.js
@@ -1,25 +1,29 @@
-function sum(array){
-    let number = 0;
-    for(i of array){
-        number = number + i
-    }
-    return number
+function feetInch(x){
+    let a = x / 12
+    let b = parseInt(a)
+    let c = x % 12
+    let d = `${b} fit ${c} inch.`
+    return d;
 }
-const array = [1,2,3,4,5,6]
-const result = sum(array);
-console.log("The sum of full array :",result);
-function even(arry){
-    let num = 0;
-    for(i of arry){
-        if(i % 2 ===0){
-            num = num + i
-        } 
-    }
-    return num;
-}
-const arry = [1,2,3,4,5,6]
-const resul = even(arry);
-console.log("The sum of EvenNumbers :", resul);
+const result = feetInch(75)
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
