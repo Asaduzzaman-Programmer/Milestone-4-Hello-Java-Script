bff816eebd9105ec11e0b6a4cb3c07033047a796
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-19 17:11:20 +0600

    just stat today

--- MIlestone.js
+++ MIlestone.js
@@ -554,7 +554,17 @@
 // }
 // const result = start(8);
 // console.log(result);
-
+// function make_avg(array, size){
+//     let sum = 0;
+//     for(let num of array){
+//         sum = sum + num
+//     }
+//     return sum / size
+// }
+// const array = [1,2,3,4,5,6,7,8,9,10]
+// const size = array.length
+// const result = make_avg(array, size)
+// console.log(result);
 
 
 
