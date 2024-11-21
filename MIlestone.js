086935e190c67d5782e33a1698a49d0b19c6d453
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-21 20:32:09 +0600

    product

--- MIlestone.js
+++ MIlestone.js
@@ -675,6 +675,24 @@
 // }
 // const result = cheap(fon);
 // console.log(result);
+// const fon = [
+//     {name: "iPhoneXr", camera: "64MP", price: 50000, battary: "4500MaH"},
+//     {name: "iPhoneXs", camera: "64MP", price: 20000, battary: "4500MaH"},
+//     {name: "iPhone12", camera: "64MP", price: 60000, battary: "4500MaH"},
+//     {name: "iPhone13", camera: "64MP", price: 80000, battary: "4500MaH"},
+//     {name: "iPhone7",  camera: "64MP", price: 10000, battary: "4500MaH"}
+// ]
+// function cheap(obb){
+//     let cheap = fon[0];
+//     for(let model of obb){
+//         if(cheap.price > model.price){
+//             cheap = model
+//         }
+//     }
+//     return cheap
+// }
+// const result = cheap(fon);
+// console.log(result);
 
 
 
--- js.js
+++ js.js
@@ -1,23 +1,19 @@
-const fon = [
-    {name: "iPhoneXr", camera: "64MP", price: 50000, battary: "4500MaH"},
-    {name: "iPhoneXs", camera: "64MP", price: 20000, battary: "4500MaH"},
-    {name: "iPhone12", camera: "64MP", price: 60000, battary: "4500MaH"},
-    {name: "iPhone13", camera: "64MP", price: 80000, battary: "4500MaH"},
-    {name: "iPhone7",  camera: "64MP", price: 10000, battary: "4500MaH"}
-]
+const pro = [
+    {name: "shirt", price: 700},
+    {name: "pant", price: 500},
+    {name: "genji", price: 300},
+    {name: "panjabi", price: 800},
+];
 
-function cheap(obb){
-    let cheap = fon[0];
-    for(let model of obb){
-        if(cheap.price > model.price){
-            cheap = model
-        }
-    }
-    return cheap
+function total(pro){
+    let itemTotal = 0;
+     for(let pri of pro){
+        itemTotal = itemTotal + pri.price
+     }
+     return itemTotal
 }
-const result = cheap(fon);
-console.log(result);
-
+ const result = total(pro);
+ console.log(result);
 
 
 
