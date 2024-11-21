e488c6d15fd9fe77fbe3d499c444b0f5f0875d9e
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-21 12:45:48 +0600

    big compare

--- MIlestone.js
+++ MIlestone.js
@@ -571,6 +571,37 @@
 // }
 // const result = work(inpu);
 // console.log(result);
+// const nam = ['abul', 'babul', 'cabul', 'babul' , 'mokbul', 'cabul']
+// const num = [1,1,2,7,8,4,7,8,5,4]
+// function noDup(arr){
+//     let uniqe = [];
+//     for(let one of arr){
+//         if(uniqe.includes(one)=== false){
+//             uniqe.push(one)
+//         }
+//     }
+//     return uniqe
+// }
+// const result = noDup(num)
+// console.log(result);
+// const num = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+// function uniqe(arr){
+//     let even = [];
+//     let evenn = []
+//     for(let one of arr){
+//         if(one % 2 === 0){
+//             even.push(one)
+//         }
+//     }
+//     for(let one of even){
+//         if(evenn.includes(one) === false){
+//             evenn.push(one)
+//         }
+//     }
+//     return evenn
+// }
+// const result = uniqe(num)
+// console.log(result);
 
 
 
--- js.js
+++ js.js
@@ -1,37 +1,28 @@
-// const nam = ['abul', 'babul', 'cabul', 'babul' , 'mokbul', 'cabul']
-// const num = [1,1,2,7,8,4,7,8,5,4]
-// function noDup(arr){
-//     let uniqe = [];
-//     for(let one of arr){
-//         if(uniqe.includes(one)=== false){
-//             uniqe.push(one)
-//         }
-//     }
-//     return uniqe
-// }
-// const result = noDup(num)
-// console.log(result);
-
-const num = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
-function uniqe(arr){
-    let even = [];
-    let evenn = []
-    for(let one of arr){
-        if(one % 2 === 0){
-            even.push(one)
-        }
-    }
-    for(let one of even){
-        if(evenn.includes(one) === false){
-            evenn.push(one)
-        }
-    }
-    return evenn
+const jim = 58;
+const kim = 85;
+const tim = 10;
+if(jim > kim && jim > tim){
+    console.log("The hero Jim");
 }
-const result = uniqe(num)
+else if(kim > jim && kim > tim){
+    console.log("The hero kim");
+}
+else {
+    console.log("The hero tim");
+}
+function big(num1, num2, num3){
+    if(num1 > num2 && num1 > num3){
+       return num1
+    }
+    else if(num2 > num1 && num2 > num3){
+        return num2;
+    }
+    else{
+        return num3
+    }
+}
+const result = big(12,2,8)
 console.log(result);
-
-
 
 
 
