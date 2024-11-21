341c8a9ac06fe71da175cb20a1533189cbbc1a74
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-21 11:04:07 +0600

    extra

--- js.js
+++ js.js
@@ -1,19 +1,35 @@
-const nam = ['abul', 'babul', 'cabul', 'babul' , 'mokbul', 'cabul']
-function noDup(arr){
-    let uniqe = [];
+// const nam = ['abul', 'babul', 'cabul', 'babul' , 'mokbul', 'cabul']
+// const num = [1,1,2,7,8,4,7,8,5,4]
+// function noDup(arr){
+//     let uniqe = [];
+//     for(let one of arr){
+//         if(uniqe.includes(one)=== false){
+//             uniqe.push(one)
+//         }
+//     }
+//     return uniqe
+// }
+// const result = noDup(num)
+// console.log(result);
+
+const num = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+function uniqe(arr){
+    let even = [];
+    let evenn = []
     for(let one of arr){
-        if(uniqe.includes(one)=== false){
-            uniqe.push(one)
+        if(one % 2 === 0){
+            even.push(one)
         }
     }
-    return uniqe
+    for(let one of even){
+        if(evenn.includes(one) === false){
+            evenn.push(one)
+        }
+    }
+    return evenn
 }
-const result = noDup(nam)
+const result = uniqe(num)
 console.log(result);
-
-
-
-
 
 
 
