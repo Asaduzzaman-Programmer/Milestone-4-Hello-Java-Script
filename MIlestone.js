acd4ba37a35a92fa08730d75eba384a465c8d26d
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-22 18:08:58 +0600

    cal

--- MIlestone.js
+++ MIlestone.js
@@ -708,6 +708,42 @@
 // }
 //  const result = total(pro);
 //  console.log(result);
+// function add(a, b ){
+//     return a + b;
+// }
+// function sub(a, b ){
+//     return a - b;
+// }
+// function multi(a, b ){
+//     return a * b;
+// }
+// function div(a, b )   {
+//     return a / b;
+// }
+// function cal(a, b, op){
+//     if(op === "+"){
+//         const result = add(a, b);
+//         return result;
+//     }
+//     else if(op === "-"){
+//         const result = sub(a, b);
+//         return result;
+//     }
+//     else if(op === "*"){
+//         const result = multi(a, b);
+//         return result;
+//     }
+//     else  if(op === "/"){
+//         const result = div(a, b);
+//         return result;
+//     }
+//     else{
+//         return "Math Error"
+//     }
+// }
+// const result = cal(5, 8, "*")
+// console.log(result);
+
 
 
 
--- js.js
+++ js.js
@@ -1,44 +1,35 @@
-function add(a, b ){
+function add(a, b){
     return a + b;
 }
-function sub(a, b ){
+function sub(a, b){
     return a - b;
 }
-function multi(a, b ){
+function multi(a, b){
     return a * b;
 }
-function div(a, b )   {
+function div(a, b){
     return a / b;
 }
 function cal(a, b, op){
     if(op === "+"){
-        const result = add(a, b);
+        const result = add(a ,b);
         return result;
     }
     else if(op === "-"){
-        const result = sub(a, b);
-        return result;
+        return sub(a, b)
     }
     else if(op === "*"){
-        const result = multi(a, b);
-        return result;
+        return multi(a, b)
     }
-    else  if(op === "/"){
-        const result = div(a, b);
-        return result;
+    else if(op === "/"){
+        return div(a, b)
     }
     else{
-        return "Math Error"
+        return "MATH Eroro"
     }
 }
-const result = cal(5, 8, "*")
+const result = cal(5, 7, "")
 console.log(result);
-
-
-
-
-
-
 
 
 
