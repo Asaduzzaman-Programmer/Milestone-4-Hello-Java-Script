85816461e0e63015d72343aa0deebe450ef423ec
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-24 18:11:08 +0600

    cal

--- js.js
+++ js.js
@@ -1,29 +1,43 @@
-function subscribed(){
-    document.getElementById('btn').innerHTML = "Done";
-    document.getElementById('btn').style.backgroundColor = '#262626';
+function btn(){
+    document.getElementById('btn').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked'
 }
 
 
-function like(){
-    document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+function add(num1, num2){
+    return num1 + num2;
+}
+function sub(num1, num2){
+    return num1 + num2; 
+}
+function multi(num1, num2){
+    return num1 * num2;
+}
+function div(num1, num2){
+    return num1 / num2;
 }
 
 
+function cal(a, b, op){
+    if(op === "+"){
+        const result = add(a, b);
+        return result;
+    }
+    else if(op === "-"){
+        const result = sub(a, b);
+        return result;
+    }
+    else if(op === "*"){
+        const result = multi(a, b);
+        return result;
+    }
+    else if(op === "/"){
+        const result = div(a, b);
+        return result;
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const result = cal(24, 5, "/")
+console.log(result);
 
 
 
