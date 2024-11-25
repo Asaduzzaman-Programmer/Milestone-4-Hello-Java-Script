95c68c6029064e794efbb69aabac5737e3e67c41
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-25 20:45:36 +0600

    number

--- MIlestone.js
+++ MIlestone.js
@@ -775,6 +775,41 @@
 // }
 // const result = cal(5, 7, "")
 // console.log(result);
+// function btn(){
+//     document.getElementById('btn').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked'
+// }
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// function sub(num1, num2){
+//     return num1 + num2; 
+// }
+// function multi(num1, num2){
+//     return num1 * num2;
+// }
+// function div(num1, num2){
+//     return num1 / num2;
+// }
+// function cal(a, b, op){
+//     if(op === "+"){
+//         const result = add(a, b);
+//         return result;
+//     }
+//     else if(op === "-"){
+//         const result = sub(a, b);
+//         return result;
+//     }
+//     else if(op === "*"){
+//         const result = multi(a, b);
+//         return result;
+//     }
+//     else if(op === "/"){
+//         const result = div(a, b);
+//         return result;
+//     }
+// }
+// const result = cal(24, 5, "/")
+// console.log(result);
 
 
 
--- js.js
+++ js.js
@@ -1,43 +1,7 @@
-function btn(){
-    document.getElementById('btn').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked'
-}
-
-
-function add(num1, num2){
-    return num1 + num2;
-}
-function sub(num1, num2){
-    return num1 + num2; 
-}
-function multi(num1, num2){
-    return num1 * num2;
-}
-function div(num1, num2){
-    return num1 / num2;
-}
-
-
-function cal(a, b, op){
-    if(op === "+"){
-        const result = add(a, b);
-        return result;
-    }
-    else if(op === "-"){
-        const result = sub(a, b);
-        return result;
-    }
-    else if(op === "*"){
-        const result = multi(a, b);
-        return result;
-    }
-    else if(op === "/"){
-        const result = div(a, b);
-        return result;
-    }
-}
-
-const result = cal(24, 5, "/")
-console.log(result);
+let a = 0.6;
+let b = 0.7;
+let c = (a*10 + b*10)/ 10
+console.log(c );
 
 
 
