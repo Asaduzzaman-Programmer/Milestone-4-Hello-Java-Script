599da7d96e4ec5611a7ce9efe0c76436846e3a11
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-27 13:35:42 +0600

    change h1 tag

--- dom.js
+++ dom.js
@@ -1,12 +1,14 @@
-const h1 = document.getElementsByTagName('h1')
+// const h1 = document.getElementsByTagName('h1')
 
-console.log(h1);
+// console.log(h1);
 
-for(let i of h1){
-    console.log(i.innerText);
-}
+// for(let i of h1){
+//     console.log(i.innerText);
+// }
 
-
+let hi = document.getElementById('hi')
+hi.innerText = "hello"
+console.log(hi);
 
 
 
