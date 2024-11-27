45e0012e25ec6dd9ff078904c80574213b5cedc1
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-27 13:39:33 +0600

    sopno

--- dom.js
+++ dom.js
@@ -1,17 +1,7 @@
-// const h1 = document.getElementsByTagName('h1')
+let sopno = document.getElementById('sopno')
 
-// console.log(h1);
-
-// for(let i of h1){
-//     console.log(i.innerText);
-// }
-
-let hi = document.getElementById('hi')
-hi.innerText = "hello"
-console.log(hi);
-
-
-
+sopno.innerText = 'Bokachoda'
+console.log(sopno);
 
 
 
