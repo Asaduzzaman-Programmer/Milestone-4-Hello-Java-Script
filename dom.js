9859ee3f79dc92c69acca3f40fa60d09d2e14241
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-27 15:33:47 +0600

    test theme

--- dom.js
+++ dom.js
@@ -1,22 +1,28 @@
 const a = document.querySelectorAll('section')
-for(let i of a){
+for (let i of a) {
     console.log(i);
     i.style.border = "2px solid red"
     i.style.margin = "5px"
     i.style.alignItems = 'center'
     i.style.textAlign = 'center'
-   
+
 }
 
-const b = document.getElementById('id')
-b.classList.add('text')
 
 
 
+function like() {
+    document.getElementById('btn').innerText = 'Liked'
+    const b = document.getElementById('id')
+    b.classList.add('text')
 
 
+    document.getElementById('id2').classList.add('text')
+    const x = document.createElement('li')
+x.innerText = 'Tasin'
+    document.getElementById('id2').appendChild(x)
 
-
+}
 
 
 
