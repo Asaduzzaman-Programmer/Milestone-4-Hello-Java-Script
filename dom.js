b8d82a2bf90c5d1147a6b82f3c50575a69aa97a0
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-27 13:48:43 +0600

    get element

--- dom.js
+++ dom.js
@@ -3,7 +3,11 @@ let sopno = document.getElementById('sopno')
 sopno.innerText = 'Bokachoda'
 console.log(sopno);
 
-
+let a = document.getElementsByClassName('bokachoda')
+for(let i of a){
+    i.innerText = "bokachoda sobai"
+    console.log(i.innerText);
+}
 
 
 
