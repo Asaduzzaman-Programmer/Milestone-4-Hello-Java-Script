e11ab3f85f217224e1691982583c5e8c0dcbf918
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-27 13:57:21 +0600

    list

--- dom.js
+++ dom.js
@@ -1,18 +1,16 @@
-let sopno = document.getElementById('sopno')
-
-sopno.innerText = 'Bokachoda'
-console.log(sopno);
-
-let a = document.getElementsByClassName('bokachoda')
-for(let i of a){
-    i.innerText = "bokachoda sobai"
-    console.log(i.innerText);
-}
+// let sopno = document.getElementById('sopno')
+// sopno.innerText = 'Bokachoda'
+// console.log(sopno);
+// let a = document.getElementsByClassName('bokachoda')
+// for(let i of a){
+//     i.innerText = "bokachoda sobai"
+//     console.log(i.innerText);
+// }
 
 
+let a = document.querySelectorAll('.name li')
 
-
-
+console.log(a);
 
 
 
