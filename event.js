a8ea53e50706e0b29b579fce814d2bf1b07d43bd
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-28 14:39:15 +0600

    make me pink

--- event.js
+++ event.js
@@ -1,1 +1,31 @@
 console.log("mal ta paiche");
+
+
+const makePink = document.getElementById('pink');
+makePink.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'pink'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
