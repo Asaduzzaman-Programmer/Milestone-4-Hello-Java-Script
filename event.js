64365a9735df441c4201802d9ce6bbf8055df42f
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-29 19:21:03 +0600

    again test changing name

--- event.js
+++ event.js
@@ -1,10 +1,4 @@
-console.log("mal ta paiche");
 
-
-const makePink = document.getElementById('pink');
-makePink.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'pink'
-})
 
 
 
