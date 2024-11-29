204824278a5dd65b47398e6798c4a8457d0a4db3
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-11-30 02:34:07 +0600

    comment

--- event.js
+++ event.js
@@ -1,13 +1,33 @@
+const makePink = document.getElementById('pink');
+makePink.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'pink'
+})
 
 
+const section = document.getElementById('sec');
+function add(){
+    const sec = document.createElement('section');
+    sec.innerHTML = `
+       
+        <ul>
+          <li>sawon</li>
+          <li>taskin</li>
+          <li>sawon</li>
+          <li>taskin</li>
+        </ul>
+     
+`
+    document.querySelector('body').append(sec)
+}
 
-
-
-
-
-
-
-
+function comment(){
+    const text =  document.getElementById('text').value
+    const commentSection = document.getElementById('comment-section')
+    const add = document.createElement('p')
+    add.innerText = text
+    commentSection.appendChild(add)
+    document.getElementById('text').value = ''
+}
 
 
 
