7576bfd7ff7aaa6a53d6de4929bcd52a138ab7f1
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-14 16:39:15 +0600

    nested

--- es6.js
+++ es6.js
@@ -1,10 +1,10 @@
 const num = [1,2,3,54,6,8]
 const max = Math.max(...num);
-console.log(max);
-console.log(...num);
+// console.log(max);
+// console.log(...num);
 
 const neww = num;
-console.log(...neww);
+// console.log(...neww);
 
 const ob = {
     name: "glass",
@@ -14,4 +14,30 @@ const ob = {
 
 const k = Object.keys(ob);
 const v = Object.values(ob)
-console.log(k, v);
+// console.log(k, v);
+
+const data = [{id: 1, name: "abul", address: "kochu khet"}];
+console.log(data[0].address);
+
+const product = {
+    name: "iPhone",
+    model:[
+        {
+            mobile: "iPhone 14",
+            ram: 8,
+            price: 156000
+        },
+        {
+            mobile: "iPhone 15",
+            ram: 8,
+            price: 166000
+        },
+        {
+            mobile: "iPhone 16",
+            ram: 8,
+            price: 197000
+        }
+    ]
+}
+
+console.log(product.model[2].mobile);
