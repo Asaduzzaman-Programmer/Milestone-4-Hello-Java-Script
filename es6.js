a756edc152ca409c317143afb3e809175aef6a01
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-14 14:28:06 +0600

    keys-value

--- es6.js
+++ es6.js
@@ -3,3 +3,15 @@ const max = Math.max(...num);
 console.log(max);
 console.log(...num);
 
+const neww = num;
+console.log(...neww);
+
+const ob = {
+    name: "glass",
+    brand : "texas",
+    isClean : true,
+}
+
+const k = Object.keys(ob);
+const v = Object.values(ob)
+console.log(k, v);
